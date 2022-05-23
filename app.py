61c7c0b7a69289f73aa6aeff6bcbbaa58eb890ce
Vivek CP <86761983+cpvivek@users.noreply.github.com>
2022-05-23 12:38:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,88 @@
+   
+import cv2
+import numpy as np
+import streamlit as st
+from tensorflow import keras
+from keras.models import model_from_json
+from keras.preprocessing.image import img_to_array
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import RTCConfiguration, VideoProcessorBase, WebRtcMode
+
+# load model
+emotion_dict = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'sad',5: 'suprise', 6: 'neutral'}
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+classifier = model_from_json(loaded_model_json)
+
+# load weights into new model
+classifier.load_weights("model.h5")
+
+#load face
+try:
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
+
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+
+class Faceemotion(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        #image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            image=img_gray, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+                prediction = classifier.predict(roi)[0]
+                maxindex = int(np.argmax(prediction))
+                finalout = emotion_dict[maxindex]
+                output = str(finalout)
+            label_position = (x, y)
+            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        return img
+
+def main():
+    # Face Emotion Application #
+    html_temp = """
+    <body style="background-color:red;">
+    <div style="background-color:blue ;padding:10px">
+    <h2 style="color:white;text-align:center;">Face Emotion Recognisation App</h2>
+    <style>#"Created by Lakshmi Narayana" {text-align: center}</style>
+    </div>
+    </body>
+    """
+  
+
+    st.markdown(html_temp, unsafe_allow_html=True)
+    st.write("Created by Lakshmi Narayana")
+    st.write("Model built from OpenCV, Custom CNN model and Streamlit")
+    st.write("**Directions**")
+    st.write('''
+                
+                1. Click on the START button to start the session.
+                
+                2. Allow the Webcam access to utilise the service. 
+        
+                3. It will predict the realtime face emotion using webcam.
+                
+                4. Click on STOP to end the session.
+                
+                ''')
+    webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,
+                        video_processor_factory=Faceemotion)
+
+
+if __name__ == "__main__":
+    main()
